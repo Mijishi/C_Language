84939c5fd9f1ec481b700169c658d943e3dd370c
Mijishi <747851977@qq.com>
2020-02-03 19:30:27 +0800

    ccc

---
+++ c语言100道/51.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	printf("请输入两个数\n");
+	scanf("%d%d",&i,&j);
+	if(i>j)
+	{
+		printf("%d大",i);	
+	}	
+	else
+	{
+		printf("%d大",j);
+	}
+} 
---
+++ c语言100道/52.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    unsigned a, b, c, d;
+    printf("ÇëÊäÈëÕûÊý£º\n");
+    scanf("%o", &a);
+    b = a >> 4;
+    c = ~(~0 << 4);
+    d = b & c;
+    printf("%o\n", d);
+    return 0;
+}
